71372f177ec5c09309a172d57cb4ab25426321ba
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-05-28 23:36:58 +0800

    added styling to nav bar on hover and active

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,19 @@
 // entry file
-import './home/style.css';
+import './home/home.css';
 import './reset.css';
+import './index.css'
 
 import { appendElements } from './home/home';
+
+const listItems = document.querySelectorAll('ul li');
+
+listItems.forEach(item => {
+    item.addEventListener('click', function() {
+        // Remove active class from all items
+        listItems.forEach(li => li.classList.remove('active'));
+        // Add active class to the clicked item
+        this.classList.add('active');
+    });
+});
 
 appendElements();
